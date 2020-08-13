160c55afcf987c84dd93e66711611fdbb79b5f69
javierc22 <javier.ec.22@gmail.com>
2020-08-13 12:18:23 -0400

    Agregar datos de prueba al seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
+puts("|- - - - - - - - - - Creando usuarios de prueba - - - - - - - - - -")
 User.create([
   {name: 'Administrador', legal_number: '123450-k',email: 'admin@email.cl', password: 'admin', password_confirmation: 'admin'},
   {name: 'Usuario prueba 1', legal_number: '123451-k',email: 'user01@email.cl', password: 'iwill2020', password_confirmation: 'iwill2020'},
@@ -13,3 +6,15 @@ User.create([
 ])
 
 User.first.add_role :admin
+
+puts("|- - - - - - - - - - Creando regiones y comunas - - - - - - - - - -")
+data = JSON.parse(File.read("public/chile_comunas_regiones.json"))
+country = Country.create(name: "Chile")
+
+data.each do |item|
+  region = Region.create(name: item["region"], country_id: country.id)
+  
+  item["comunas"].each do |commune|
+    Commune.create(name: commune, region_id: region.id)
+  end
+end
